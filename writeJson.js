b14f8e1c81768777a2ef35211a408f81fe3eb3ca
Sanyam Arya <er.sanyam.arya@gmail.com>
2017-05-12 11:57:43 +0530

    Update writeJson.js

--- writeJson.js
+++ writeJson.js
@@ -1,4 +1,22 @@
-var hardware = { // creating a js object
+/*....................................................
+* Author: Sanyam Arya
+* ersanyamarya@gmail.com
+* https://www.facebook.com/er.sanyam.arya
+* https://www.linkedin.com/in/sanyam-arya-077ab638/
+......................................................*/
+
+{
+    "hardwareid": "123-TEST-45",
+    "type": "Device Measurements",
+    "requests": {
+        "Measurements": {
+            "temperature": 23,
+            "humidity": 17
+        }
+    },
+    "updateState": true,
+    "eventDate": "2017-05-03T05:55:17.075Z"
+}var hardware = { // creating a js object
   hardwareid: "123-TEST-45", //properties
   type: "Device Measurements",
   requests: { // sub object
