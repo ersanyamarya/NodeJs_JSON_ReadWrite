b15efd974baab742fb09c21ab63f0c442cf73f9b
Sanyam Arya <er.sanyam.arya@gmail.com>
2017-05-12 11:56:51 +0530

    Update readJson.js

--- readJson.js
+++ readJson.js
@@ -1,3 +1,9 @@
+/*....................................................
+* Author: Sanyam Arya
+* ersanyamarya@gmail.com
+* https://www.facebook.com/er.sanyam.arya
+* https://www.linkedin.com/in/sanyam-arya-077ab638/
+......................................................*/
 //read from the same file in which out prom the write file is saved
 var fs = require("fs"); //importing fs
 var data = fs.readFileSync("json.txt"); //reading file
